5945cb76c49a65539e06e9d26fd9b477c299df77
Mike Echevarria <mechevar@redhat.com>
2019-02-14 14:58:10 -0500

    add task management calls

--- angular/src/app/detail/kie.service.ts
+++ angular/src/app/detail/kie.service.ts
@@ -4,9 +4,7 @@ import { catchError } from 'rxjs/operators';
 import { MessageService } from '../message/message.service';
 import { Observable, of } from 'rxjs/index';
 
-const headers: HttpHeaders = new HttpHeaders()
-.append('Content-Type', 'application/json')
-.append('Authorization', 'Basic ' + btoa('pamAdmin:redhatpam1!'));
+const headers: HttpHeaders = new HttpHeaders().append('Content-Type', 'application/json').append('Authorization', 'Basic ' + btoa('pamAdmin:redhatpam1!'));
 
 const httpOptions = {
   headers: headers
@@ -19,7 +17,6 @@ const containerId = 'RestCall_1.0.0';
   providedIn: 'root'
 })
 export class KieService {
-
   constructor(private messageService: MessageService, private http: HttpClient) {}
 
   process(id: number, annualRent: number): Observable<any> {
@@ -32,6 +29,55 @@ export class KieService {
     return this.http.post<any>(url, body, httpOptions).pipe(
       catchError(res => {
         return this.handleError('process()', res);
+      })
+    );
+  }
+
+  getTasks(): Observable<any> {
+    const url = `${baseUrl}/server/queries/tasks/instances/pot-owners`;
+    return this.http.get<any>(url, httpOptions).pipe(
+      catchError(res => {
+        return this.handleError('getTasks()', res);
+      })
+    );
+  }
+
+  getTask(taskId: number): Observable<any> {
+    const url = `${baseUrl}/server/queries/tasks/instances/${taskId}`;
+    return this.http.get<any>(url, httpOptions).pipe(
+      catchError(res => {
+        return this.handleError('getTask()', res);
+      })
+    );
+  }
+
+  claim(taskId: number): Observable<any> {
+    const url = `${baseUrl}/server/containers/${containerId}/tasks/${taskId}/states/claimed`;
+    return this.http.put<any>(url, null, httpOptions).pipe(
+      catchError(res => {
+        return this.handleError('claim()', res);
+      })
+    );
+  }
+
+  start(taskId: number): Observable<any> {
+    const url = `${baseUrl}/server/containers/${containerId}/tasks/${taskId}/states/started`;
+    return this.http.put<any>(url, null, httpOptions).pipe(
+      catchError(res => {
+        return this.handleError('start()', res);
+      })
+    );
+  }
+
+  complete(taskId: number, status: string): Observable<any> {
+    const url = `${baseUrl}/server/containers/${containerId}/tasks/${taskId}/states/completed`;
+    const body = {
+      updatedstatus: status
+    };
+
+    return this.http.put<any>(url, body, httpOptions).pipe(
+      catchError(res => {
+        return this.handleError('complete()', res);
       })
     );
   }
