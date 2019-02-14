567b1893ef50cbd95d25fc447c80001d885c7e6a
Mike Echevarria <mechevar@redhat.com>
2019-02-13 19:51:53 -0500

    refactor to processInstanceId

--- eap/src/main/java/org/redhat/api/lease/LeaseModel.java
+++ eap/src/main/java/org/redhat/api/lease/LeaseModel.java
@@ -13,7 +13,6 @@ import java.sql.Date;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class LeaseModel implements Serializable {
 
-
   private static final long serialVersionUID = -7384751271101505737L;
 
   @Id
@@ -30,79 +29,102 @@ public class LeaseModel implements Serializable {
   private String leaseName;
   private String status;
   private Date lastUpdateDate;
-  private int processId;
+  private int processInstanceId;
 
   // everything below is a generated getter/setter
 
   public long getId() {
     return this.id;
   }
+
   public void setId(long id) {
     this.id = id;
   }
-  public int getProcessId() {
-    return this.processId;
+
+  public int getProcessInstanceId() {
+    return this.processInstanceId;
   }
-  public void setProcessId(int processId) {
-    this.processId = processId;
+
+  public void setProcessInstanceId(int processInstanceId) {
+    this.processInstanceId = processInstanceId;
   }
+
   public Date getLastUpdateDate() {
     return this.lastUpdateDate;
   }
+
   public void setLastUpdateDate(Date lastUpdateDate) {
     this.lastUpdateDate = lastUpdateDate;
   }
+
   public String getStatus() {
     return this.status;
   }
+
   public void setStatus(String status) {
     this.status = status;
   }
+
   public String getLeaseName() {
     return this.leaseName;
   }
+
   public void setLeaseName(String leaseName) {
     this.leaseName = leaseName;
   }
+
   public int getAnnualRent() {
     return this.annualRent;
   }
+
   public void setAnnualRent(int annualRent) {
     this.annualRent = annualRent;
   }
+
   public String getState() {
     return this.state;
   }
+
   public void setState(String state) {
     this.state = state;
   }
+
   public float getLat() {
     return this.lat;
   }
+
   public void setLat(float lat) {
     this.lat = lat;
   }
+
   public float getLon() {
     return this.lon;
   }
+
   public void setLon(float lon) {
     this.lon = lon;
   }
+
   public String getLeaseNumber() {
     return this.leaseNumber;
   }
+
   public void setLeaseNumber(String leaseNumber) {
     this.leaseNumber = leaseNumber;
   }
+
   public String getCity() {
     return this.city;
   }
+
   public void setCity(String city) {
     this.city = city;
   }
+
   public String getAddress() {
     return this.address;
   }
+
   public void setAddress(String address) {
     this.address = address;
   }
