d7d1f3ece0d6388c99f840c055bc1a394d22e22d
Mike Echevarria <mechevar@redhat.com>
2019-02-14 14:58:41 -0500

    add env var for 3scale

--- angular/run-local.sh
+++ angular/run-local.sh
@@ -3,5 +3,6 @@
 export PORT=4200
 export EAP="http://eap-app-gsa.apps.ocp.integration.redhatgov.io"
 export KIE="http://rhpam7-install-kieserver-gsa.apps.ocp.integration.redhatgov.io"
+export SCALE="https://lease-inventory-3scale-apicast-production.apim.apps.ocp.integration.redhatgov.io"
 
 npm run start
