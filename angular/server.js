6a8ce1651d48f2dc7147ba21effd9b8060c1547a
Mike Echevarria <mechevar@redhat.com>
2019-02-14 15:05:30 -0500

    use external 3scale route

--- angular/server.js
+++ angular/server.js
@@ -13,7 +13,7 @@ let app = express();
 app.set('port', process.env.PORT || 8080);
 app.set('eap', process.env.EAP || 'http://eap-app:8080');
 app.set('kie', process.env.KIE || 'http://rhpam7-install-kieserver:8080');
-app.set('3scale', process.env.SCALE || 'http://apicast-production:8080');
+app.set('3scale', process.env.SCALE || 'https://lease-inventory-3scale-apicast-production.apim.apps.ocp.integration.redhatgov.io');
 
 app.use(compression());
 app.use(logger('combined'));
